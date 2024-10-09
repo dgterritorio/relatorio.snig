fee85068ff2ed319bbd3d1ba4d30382acdaef55c
Massimo Manghi <massimo.manghi@gmail.com>
2024-10-09 13:23:19 +0000

    removed duplicate line, disable TCP

--- monitor/ngis_monitor_conf.tcl
+++ monitor/ngis_monitor_conf.tcl
@@ -9,19 +9,18 @@ namespace eval ::ngis {
     variable SERVICE_STATUS     "testsuite.service_status"
     variable PORT               "5432"
     variable COLUMN_NAMES       "gid,uuid,uri,entity,description,uri_type,version"
-    variable SERVICE_STATUS     "testsuite.service_status"
     variable SERVICE_LOG        "testsuite.service_log"
     variable TIMEZONE           "Europe/Lisbon"
+
     variable data_root          [file join / tmp snig]
 
     variable max_workers_number 50
     variable unix_socket_name   /tmp/ngis.socket
-    variable end_of_answer      "----"
     variable rescheduling_delay 100
     variable curldir            "" 
 
-    variable tcpaddr            127.0.0.1
-    variable tcpport            4422
+    variable tcpaddr            ""
+    variable tcpport            ""
 }
 
 package provide ngis::conf 1.1
