1e734cd49fb410330c8881179aae3b0a4006f020
Massimo Manghi <massimo.manghi@gmail.com>
2024-10-09 22:30:08 +0000

    handling auto_path in global test launch script, set run_client.tcl executable

--- monitor/utils/general_test.tcl
+++ monitor/utils/general_test.tcl
@@ -1,11 +1,18 @@
 #!/usr/bin/tclsh
 
-set dot [lsearch $auto_path "."]
-if {$dot < 0} {
-    set auto_path [concat "." $auto_path]
-} elseif {$dot > 0} {
-    set auto_path [concat "." [lreplace $auto_path $dot $dot]]
+# assuming the monitor configuration is in the parent directory
+set current_dir [file normalize [file join [file dirname [info script]] ..]]
+
+cd $current_dir
+
+set curr_dir_pos [lsearch $auto_path $current_dir]
+if {$curr_dir_pos < 0} {
+    set auto_path [concat $current_dir $auto_path]
+} elseif {$current_dir_pos > 0} {
+    set auto_path [concat $current_dir [lreplace $auto_path $current_dir_pos $current_dir_pos]]
 }
+
+package require syslog
 package require unix_sockets
 package require tdbc
 package require tdbc::postgres
@@ -28,7 +35,6 @@ proc socket_readable {con} {
     incr ::next_entity
 }
 
-
 set sql "select entity,count(ul.gid) cnt from testsuite.uris_long ul group by entity order by cnt asc"
 set resultset [::ngis::service exec_sql_query $sql]
 
@@ -36,6 +42,7 @@ set con [unix_sockets::connect $::ngis::unix_socket_name]
 chan event $con readable [namespace code [list socket_readable $con]]
 #puts "found [$resultset rowcount] rows"
 
+set nentities 0
 set entities [$resultset allrows -as dicts]
 foreach ent $entities {
 
@@ -52,5 +59,8 @@ foreach ent $entities {
     after 100
 
     vwait ::next_entity
+    incr nentities
 
+    #if {$nentities > 10} { break }
 }
+syslog -ident snig -facility user info "Sent request for checks of $nentities entities"
