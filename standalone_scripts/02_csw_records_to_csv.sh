50883208130453357fbdcc6c36ad1c70950d68cc
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-01 19:12:36 +0000

    Make the script work regardless of where it runs

--- standalone_scripts/02_csw_records_to_csv.sh
+++ standalone_scripts/02_csw_records_to_csv.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-rm CSW_RECORDS_CSV/*.csv
+BASEFOLDER="/tmp"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-FILES="CSW_RECORDS/*.xml"
+# Remove existing files
+rm -f "$BASEFOLDER/CSW_RECORDS_CSV/"*.csv
+# Ensure necessary directory exists
+mkdir -p "$BASEFOLDER/CSW_RECORDS_CSV"
+
+#rm CSW_RECORDS_CSV/*.csv
+
+FILES="$BASEFOLDER/CSW_RECORDS/*.xml"
 for f in $FILES
 do
 
@@ -11,8 +19,8 @@ filename_no_ext="${filename%.*}"
 
   echo "Processing $filename"
 
-xmlstarlet select -N dc="http://purl.org/dc/elements/1.1/" -N csw="http://www.opengis.net/cat/csw/2.0.2" -N dct="http://purl.org/dc/terms/" -N ows="http://www.opengis.net/ows" -N geonet="http://www.fao.org/geonetwork" -N xsi="http://www.w3.org/2001/XMLSchema-instance" -T -t -m "//csw:Record" -v "dc:identifier" -o '$' -v "dc:date" -o '$' --var linebreak -n --break -v "translate(dc:title, \$linebreak ,'@')" -o '$' -v "dc:type" -o '$' -m "dc:subject" -v "concat(.,',')" -b -o '$' -m "dc:format" -v "concat(.,',')" -b -o '$' -m "dct:modified" -v "concat(.,',')" -b -o '$' -v "translate(dct:abstract, \$linebreak ,'@')" -o '$' -v "translate(dc:description, \$linebreak ,'@')" -o '$' -m "dc:rights" -v "concat(.,',')" -b -o '$' -m "dc:language" -v "concat(.,',')" -b -o '$' -v "translate(dc:source, \$linebreak ,'@')" -o '$' -v "dc:URI[1]" -o '$' -v "dc:URI[2]" -o '$' -v "dc:URI[3]" -o '$' -v "ows:BoundingBox[1]/@crs" -o '$' -v "ows:BoundingBox[1]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[1]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[2]/@crs" -o '$' -v "ows:BoundingBox[2]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[2]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[3]/@crs" -o '$' -v "ows:BoundingBox[3]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[3]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[4]/@crs" -o '$' -v "ows:BoundingBox[4]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[4]/ows:UpperCorner[1]" -n CSW_RECORDS/$filename > CSW_RECORDS_CSV/$filename_no_ext.csv
+xmlstarlet select -N dc="http://purl.org/dc/elements/1.1/" -N csw="http://www.opengis.net/cat/csw/2.0.2" -N dct="http://purl.org/dc/terms/" -N ows="http://www.opengis.net/ows" -N geonet="http://www.fao.org/geonetwork" -N xsi="http://www.w3.org/2001/XMLSchema-instance" -T -t -m "//csw:Record" -v "dc:identifier" -o '$' -v "dc:date" -o '$' --var linebreak -n --break -v "translate(dc:title, \$linebreak ,'@')" -o '$' -v "dc:type" -o '$' -m "dc:subject" -v "concat(.,',')" -b -o '$' -m "dc:format" -v "concat(.,',')" -b -o '$' -m "dct:modified" -v "concat(.,',')" -b -o '$' -v "translate(dct:abstract, \$linebreak ,'@')" -o '$' -v "translate(dc:description, \$linebreak ,'@')" -o '$' -m "dc:rights" -v "concat(.,',')" -b -o '$' -m "dc:language" -v "concat(.,',')" -b -o '$' -v "translate(dc:source, \$linebreak ,'@')" -o '$' -v "dc:URI[1]" -o '$' -v "dc:URI[2]" -o '$' -v "dc:URI[3]" -o '$' -v "ows:BoundingBox[1]/@crs" -o '$' -v "ows:BoundingBox[1]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[1]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[2]/@crs" -o '$' -v "ows:BoundingBox[2]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[2]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[3]/@crs" -o '$' -v "ows:BoundingBox[3]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[3]/ows:UpperCorner[1]" -o '$' -v "ows:BoundingBox[4]/@crs" -o '$' -v "ows:BoundingBox[4]/ows:LowerCorner[1]" -o '$' -v "ows:BoundingBox[4]/ows:UpperCorner[1]" -n "$BASEFOLDER/CSW_RECORDS/$filename" > "$BASEFOLDER/CSW_RECORDS_CSV/$filename_no_ext.csv"
 
 done
 
-cat CSW_RECORDS_CSV/csw_records_*.csv > CSW_RECORDS_CSV/csw_records_csv.csv
+cat "$BASEFOLDER/CSW_RECORDS_CSV/"csw_records_*.csv > "$BASEFOLDER/CSW_RECORDS_CSV/"csw_records_csv.csv
