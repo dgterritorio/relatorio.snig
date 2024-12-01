ae47441a4f7e978755ef5fa8e9af5f0e42f70fa1
Massimo Manghi <mxmanghi@apache.org>
2024-12-02 00:33:04 +0100

    Add class for webservice returning formatted reports (accordingly to the current template), add generic class for Ajax web services

---
+++ monitor/website/tcl/reports.tcl
@@ -0,0 +1,49 @@
+# reports.tcl --
+#
+# web service to return formatted reports from the server
+#
+package require ngis::webservice
+package require ngis::servicedb
+package require yajltcl
+
+namespace eval ::rwpage {
+    ::itcl::class SnigReports {
+        inherit SnigWebService
+
+        private variable json_o
+        private variable report_n
+        private variable data
+        private variable fmtns
+
+        constructor {key} {SnigWebService::constructor $key} {
+            set json_o [yajl create [namespace current]::json -beautify 1]
+            set fmtns [[::rivetweb::RWTemplate::template $::rivetweb::template_key] formatters_ns]
+        }
+
+        public method webservice {language argsqs} {
+            $json_o reset 
+            # if we're here the 'report' argument is defined
+            set report_n [dict get $argsqs report]
+            switch $report_n {
+                118 {
+                    set data [lindex [::ngis::service service_data [dict get $argsqs gid]] 0]
+                }
+                default {
+
+                }
+            }
+        }
+
+        public method print_content {language args} {
+            switch $report_n {
+                118 {
+                    $json_o map_open string code string "618"
+                    $json_o string title string [dict get $data description]
+                    $json_o string report string [${fmtns}::service_tasks $data]
+                    $json_o map_close
+                }
+            }
+            puts [$json_o get]
+        }
+    }
+}
