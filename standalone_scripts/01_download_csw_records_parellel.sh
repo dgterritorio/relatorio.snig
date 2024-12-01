1346d2080e39a15dc0fd50f42e3df55815aa40f6
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-01 18:30:34 +0000

    Maje the script works regardless of where it runs

--- standalone_scripts/01_download_csw_records_parellel.sh
+++ standalone_scripts/01_download_csw_records_parellel.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-if [ "$1" == "identify" ]; then
-    echo "{CWSCheck} {compare CSW records to template}"
-    exit 0
-fi
+BASEFOLDER="/tmp"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
+# Export the variables so that it's available in the parallel processes
+export BASEFOLDER
+export SCRIPT_DIR
+
+# Remove existing files
+rm -f "$BASEFOLDER/CSW_RECORDS/*.xml"
 # Ensure necessary directory exists
-mkdir -p CSW_RECORDS
+mkdir -p "$BASEFOLDER/CSW_RECORDS"
 
 # Function to process each chunk
 process_chunk() {
@@ -19,25 +23,24 @@ process_chunk() {
     local payload_file="download_csw_records_payload_${start}.txt"
 
     # Prepare payload file
-    cp 01_download_csw_records_payload_template.txt "$payload_file"
-    sed -i 's/XXX/'$start'/' "$payload_file"
-    sed -i 's/YYY/'$step'/' "$payload_file"
+    cp "$SCRIPT_DIR/01_download_csw_records_payload_template.txt" "$BASEFOLDER/$payload_file"
+    sed -i 's/XXX/'$start'/' "$BASEFOLDER/$payload_file"
+    sed -i 's/YYY/'$step'/' "$BASEFOLDER/$payload_file"
 
     # Make the HTTP request and save the output
-    curl -X POST --header "Content-Type:text/xml;charset=UTF-8" --data @"$payload_file" \
+    curl -X POST --header "Content-Type:text/xml;charset=UTF-8" --data @"$BASEFOLDER/$payload_file" \
         "https://snig.dgterritorio.gov.pt/rndg/srv/por/csw?request=GetRecords&service=CSW" \
-        -o "CSW_RECORDS/csw_records_${start}_$((start + step)).xml"
+        -o "$BASEFOLDER/CSW_RECORDS/csw_records_${start}_$((start + step)).xml"
 
     # Cleanup temporary file
-    rm -f "$payload_file"
+    rm -f "$BASEFOLDER/$payload_file"
 }
 
 # Export the function for use with GNU parallel
 export -f process_chunk
 
 # Remove existing files
-rm -f CSW_RECORDS/*.xml
-rm -f download_csw_records_payload_*.txt
+#rm -f download_csw_records_payload_*.txt
 
 # Generate sequence of starting indices and run in parallel
-seq 1 50 18000 | parallel --jobs 4 process_chunk {}
+seq 1 10 18000 | parallel --jobs 4 process_chunk {}
