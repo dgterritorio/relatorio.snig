cc3b4781df374f2da53cbb194c3c1b99266a6957
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-02 06:34:53 +0000

    Make the script work regardless of where it runs

--- standalone_scripts/03_download_geonetwork_metadata.sh
+++ standalone_scripts/03_download_geonetwork_metadata.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-rm SNIG_GEONETWORK_METADATA/*.xml
+BASEFOLDER="/tmp"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+# Remove existing files
+rm -f "$BASEFOLDER/SNIG_GEONETWORK_METADATA/"*.xml
+# Ensure necessary directory exists
+mkdir -p "$BASEFOLDER/SNIG_GEONETWORK_METADATA"
 
 while IFS="$" read -r uuid other_fields
 do
 
-if [ -e SNIG_GEONETWORK_METADATA/$uuid.xml ]; then
-
-	if [ -s SNIG_GEONETWORK_METADATA/$uuid.xml ]; then
-
-	echo "Metadata file already exists and has not 0 size, skipping"
-
-	else
-
- 	echo "Re-Downloading SNIG/GEONETWORK metadata for record $uuid"
-  	wget "https://snig.dgterritorio.gov.pt/rndg/srv/api/records/$uuid/formatters/xml" -O "SNIG_GEONETWORK_METADATA/$uuid.xml"
-
-	fi
-
-else
-
+#if [ -e "$BASEFOLDER/SNIG_GEONETWORK_METADATA/$uuid.xml" ]; then
+#       if [ -s "$BASEFOLDER/SNIG_GEONETWORK_METADATA/$uuid.xml" ]; then
+#       echo "Metadata file already exists and has not 0 size, skipping"
+#               else
+#       echo "Re-Downloading SNIG/GEONETWORK metadata for record $uuid"
+#       wget "https://snig.dgterritorio.gov.pt/rndg/srv/api/records/$uuid/formatters/xml" -O "SNIG_GEONETWORK_METADATA/$uuid.xml"
+#       fi
+#else
   echo "Downloading SNIG/GEONETWORK metadata for record $uuid"
-  wget "https://snig.dgterritorio.gov.pt/rndg/srv/api/records/$uuid/formatters/xml" -O "SNIG_GEONETWORK_METADATA/$uuid.xml"
+  wget "https://snig.dgterritorio.gov.pt/rndg/srv/api/records/$uuid/formatters/xml" -O "$BASEFOLDER/SNIG_GEONETWORK_METADATA/$uuid.xml"
+#fi
 
-fi
-
-done < CSW_RECORDS_CSV/csw_records_csv.csv
+done < "$BASEFOLDER/CSW_RECORDS_CSV/csw_records_csv.csv"
