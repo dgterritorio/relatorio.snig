6c932d7c7b28aeedb764d1f134f1643b761afebe
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-03 12:46:39 +0000

    Update create_tables_and_views.sql

--- tables_and_views/create_tables_and_views.sql
+++ tables_and_views/create_tables_and_views.sql
@@ -1,6 +1,40 @@
--- CREATE TABLE TO STORE GEONETWORK METADATA IN XML FORMAT
+CREATE SCHEMA testsuite;
+CREATE SCHEMA stats;
 
-CREATE TABLE testsuite.xml_metadata (
+-- CREATE TABLE TO STORE THE LATESTS status OF THE MONITOR
+CREATE TABLE IF NOT EXISTS testsuite.service_status
+(
+    gid integer NOT NULL,
+    ts timestamp without time zone NOT NULL,
+    task character varying NOT NULL,
+    exit_status character varying NOT NULL,
+    exit_info text,
+    uuid character varying,
+    CONSTRAINT service_status_unique UNIQUE (gid, task)
+)
+
+-- CREATE TABLE TO STORE THE log OF THE MONITOR
+CREATE TABLE IF NOT EXISTS testsuite.service_log
+(
+    entryid SERIAL,
+    gid integer NOT NULL,
+    ts timestamp without time zone NOT NULL,
+    task character varying NOT NULL,
+    exit_status character varying NOT NULL,
+    exit_info text,
+    uuid character varying
+)
+
+-- CREATE TABLE TO STORE THE entities LIST
+CREATE TABLE IF NOT EXISTS testsuite.entities
+(
+    eid SERIAL,
+    description character varying,
+    CONSTRAINT entities_pk PRIMARY KEY (eid)
+)
+
+-- CREATE TABLE TO STORE GEONETWORK METADATA IN XML FORMAT
+CREATE TABLE IF NOT EXISTS testsuite.xml_metadata (
     id SERIAL PRIMARY KEY,
     filename TEXT NOT NULL,
     content XML,
