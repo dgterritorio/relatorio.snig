2d3ebfdb86169dd5527a6b7ab065dfc798e4340a
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-06 13:04:07 +0000

    Update stats_and_metrics.sql

--- tables_and_views/stats_and_metrics.sql
+++ tables_and_views/stats_and_metrics.sql
@@ -1,4 +1,4 @@
--- Count the URLs by they http protocal (http vs https vs other)
+-- Count the URLs by they http protocol (http vs https vs other)
 CREATE OR REPLACE VIEW stats_and_metrics._01_group_urls_by_http_protocol AS
 WITH temp AS 
 (
@@ -10,3 +10,23 @@ ORDER BY url_start
 )
 SELECT row_number() OVER () AS gid, * FROM temp;
 
+
+-- Count the URLs by they http status code
+CREATE OR REPLACE VIEW stats_and_metrics._02_group_by_http_status_code_global AS
+WITH temp AS 
+(
+SELECT left(exit_info,21) AS status_code, 
+count(*) AS count
+FROM testsuite.service_status
+WHERE task = 'url_status_codes' AND exit_info LIKE 'http_status_code: 200%'
+GROUP BY status_code
+UNION
+SELECT exit_info AS status_code,
+count(*) AS count
+FROM testsuite.service_status
+WHERE task = 'url_status_codes' AND exit_info NOT LIKE 'http_status_code: 200%'
+GROUP BY status_code
+
+)
+SELECT row_number() OVER () AS gid, * FROM temp
+ORDER BY gid;
