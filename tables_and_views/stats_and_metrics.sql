1cdae70e44c02f5fcf91c55533a886159c3b2cf6
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2024-12-06 12:13:55 +0000

    Create stats_and_metrics.sql

---
+++ tables_and_views/stats_and_metrics.sql
@@ -0,0 +1,12 @@
+-- Count the URLs by they http protocal (http vs https vs other)
+CREATE OR REPLACE VIEW stats_and_metrics._01_group_urls_by_http_protocol AS
+WITH temp AS 
+(
+SELECT REGEXP_REPLACE("uri_original", ':.*$', '', 'g') AS url_start, 
+count(*) AS count
+FROM testsuite.uris_long
+GROUP BY url_start
+ORDER BY url_start
+)
+SELECT row_number() OVER () AS gid, * FROM temp;
+
