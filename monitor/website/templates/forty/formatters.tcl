31cdb65cce8253664ed106eba13c0045142c5383
Massimo Manghi <mxmanghi@apache.org>
2024-12-23 19:03:37 +0100

    add entity service records table

--- monitor/website/templates/forty/formatters.tcl
+++ monitor/website/templates/forty/formatters.tcl
@@ -1,25 +1,37 @@
 package require ngis::common
+package require ngis::utils
 
-proc entities_table {rows_l} {
-    set captions_l [lmap t [list "eid" "entity definition" "service record"] { ::rivet::xml $t th }]
-    set table_head_t "<thead><tr>[join $captions_l ""]</tr></thead>"
+proc mk_table {table_captions_l table_body_rows_l {top_cap ""}} {
+    if {[string length [string trim $top_cap]] > 0} {
+        set top_cap_html [::rivet::xml $top_cap tr [list th colspan [llength $table_captions_l]]]
+    } else {
+        set top_cap_html ""
+    }
 
-    set table_body_l [lmap r $rows_l {
+    set captions_html_l [lmap t $table_captions_l { ::rivet::xml $t th }]
+    set table_head_html "<thead>${top_cap_html}[join $captions_html_l ""]</thead>"
+
+    set table_body_l [lmap r $table_body_rows_l {
         ::rivet::xml [join [lmap e $r { ::rivet::xml $e td }] ""] tr
     }]
+    set table_body_html [::rivet::xml [join $table_body_l "\n"] tbody]
+    return [::rivet::xml "${table_head_html}\n${table_body_html}" [list table class "table-wrapper"]]
+}
 
-    set table_body_t [::rivet::xml [join $table_body_l "\n"] tbody]
-
-    return [::rivet::xml "${table_head_t}\n${table_body_t}" [list table class "table-wrapper"]]
+proc entities_table {rows_l} {
+    return [mk_table [list "eid" "entity definition" "service record"] $rows_l ""]
 }
 
 proc entity_service_recs {rows_l entity_description} {
-    set captions_l $::ngis::reports::report_a(122.capts)
-    set captions_html_l [lmap t $::ngis::reports::report_a(122.capts) { ::rivet::xml $t th }]
-    set table_head_t0 [::rivet::xml $entity_description tr [list td colspan [llength $captions_l]]]
-    set table_head_t1 [::rivet::xml $captions_html_l ""
+    set services_t [lmap s $rows_l {
+        dict with s {
+            set uri_d   [::uri::split $uri]
+            set host    [dict get $uri_d host]
 
-
-
-    return [::rivet::xml [$hr_formatter c122 $rows_l $entity_description] pre]
+            if {![info exists version]} { set version "" }
+            set r [list $gid [::ngis::utils::string_truncate $description 60] $host $uri_type $version]
+        }
+        set r
+    }]
+    return [mk_table {*}$::ngis::reports::report_a(122.capts) $services_t "\[122\] $entity_description"]
 }
