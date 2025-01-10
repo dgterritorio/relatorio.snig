ed53735feab96c024687469af5b79e7f8e08567b
Massimo Manghi <mxmanghi@apache.org>
2025-01-10 13:43:48 +0100

    fixed uri test in tasks/00_congruence.tcl

--- monitor/tasks/00_congruence.tcl
+++ monitor/tasks/00_congruence.tcl
@@ -18,9 +18,21 @@
                         return [::ngis::tasks::make_warning_result "undefined_$p" "" "Undefined description"]
                     }
                 }
+                uri_original -
                 uri {
+                    set uri_re {^(((ht|f)tp(s?))\://)?(www.|[a-zA-Z].)[a-zA-Z0-9\-\.]+\.([a-zA-z]*)(\:[0-9]+)*(/($|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$#\=~_\-]+))*$}
+
+                    if {[dict exists $job_d $p]} {
+                        if {[regexp $uri_re [dict get $job_d $p]] == 0} {
+                            return [::ngis::tasks::make_error_result "invalid_uri" "" "Invalid uri for gid [dict get $job_d gid]"]
+                        }
+                    } else {
+                        return [::ngis::tasks::make_error_result "undefined_uri" "" "Undefined uri for gid [dict get $job_d gid]"]
+                    }
+
+
                     if {!([dict exists $job_d $p] && ([dict get $job_d $p] != ""))} {
-                        return [::ngis::tasks::make_error_result "missing_url" "" "Undefined url for gid [$job_d gid]"]
+                        return [::ngis::tasks::make_error_result "missing_url" "" "Undefined url for gid [dict get $job_d gid]"]
                     }
                 }
             }
