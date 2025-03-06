57f69bdc710d0c70ba1aa24c4a54c9d4af1c9314
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-03-06 08:13:58 +0000

    Update 00_harvest_import_and_update.sh
    Wrap all scripts in a single one.

--- standalone_scripts/00_harvest_import_and_update.sh
+++ standalone_scripts/00_harvest_import_and_update.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+BASEFOLDER="/tmp"
+SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
+
+$SCRIPT_DIR/01_download_csw_records_parallel.sh
+$SCRIPT_DIR/02_csw_records_to_csv.sh
+$SCRIPT_DIR/03_download_geonetwork_metadata_parallel.sh
+$SCRIPT_DIR/04_import_xml_metadata.sh
+$SCRIPT_DIR/05_geonetwork_metadata_to_csv.sh
+$SCRIPT_DIR/06_add_type_and_version.sh
+$SCRIPT_DIR/07_import_and_update_data.sh
