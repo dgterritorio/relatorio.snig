faff8fe2293e2f8680766362ba019d95bbf86204
Massimo Manghi <mxmanghi@apache.org>
2025-03-13 17:02:51 +0100

    add client I/O library, extending features of URI test procedure

---
+++ monitor/utils/client_io.tcl
@@ -0,0 +1,147 @@
+# client_io.tcl
+#
+# base procedure for implementing client operations
+
+package require json
+
+# assuming the monitor configuration is in the parent directory
+set current_dir [file normalize [file join [file dirname [info script]] ..]]
+
+cd $current_dir
+
+set curr_dir_pos [lsearch $auto_path $current_dir]
+if {$curr_dir_pos < 0} {
+    set auto_path [concat $current_dir $auto_path]
+} elseif {$current_dir_pos > 0} {
+    set auto_path [concat $current_dir [lreplace $auto_path $current_dir_pos $current_dir_pos]]
+}
+
+package require syslog
+package require unix_sockets
+package require ngis::conf
+
+namespace eval ::ngis::clientio {
+    variable json_txt            ""
+    variable expected_code       0
+    variable protocol_status     INIT
+    variable returned_message_d  ""
+    variable wait_v
+
+    variable connection
+
+    proc reset_proto {code} {
+        variable json_txt
+        variable expected_code
+        variable protocol_status
+        variable wait_v
+
+        set protocol_status READING
+        set expected_code $code
+        set json_txt ""
+        set wait_v  0
+    }
+
+    proc open_connection {{usocket ""}} {
+        variable connection
+        if {$usocket == ""} {
+            set usocket $::ngis::unix_socket_name
+        }
+        set con [unix_sockets::connect $::ngis::unix_socket_name]
+        chan event $con readable [namespace code [list [namespace current]::socket_readable $con]]
+        set connection $con
+        return $connection
+    }
+    namespace export open_connection
+
+    proc set_protocol_status {new_st} {
+        variable protocol_status
+        variable wait_v
+
+        set protocol_status $new_st
+        incr wait_v
+    }
+
+    proc socket_readable {con} {
+        variable expected_code
+        variable protocol_status
+        variable json_txt
+        variable returned_message_d
+
+        if {[chan eof $con]} {
+            puts "eof detected"
+            chan close $con
+            return
+        }
+
+        append json_txt [chan gets $con]
+
+        if {[catch { set returned_message_d [::json::json2dict $json_txt] }]} {
+            set_protocol_status READING
+            return
+        }
+
+        #puts "returned code: $returned_message_d (expected $expected_code)"
+
+        if {[dict get $returned_message_d code] == $expected_code} {
+            set_protocol_status OK
+        } else {
+            set_protocol_status [list [dict get $returned_message_d code] "protocol error"]
+        }
+    }
+
+    proc send_to_server {connection client_message expcode {waittime 400}} {
+        reset_proto $expcode
+        chan puts $connection $client_message
+        chan flush $connection
+        after $waittime
+    }
+    namespace export send_to_server
+
+    proc read_protocol_status {} {
+        variable protocol_status
+
+        return $protocol_status
+    }
+    namespace export read_protocol_status
+
+    proc read_result_code {} {
+        variable returned_message_d
+
+        if {[dict exists $returned_message_d code]} {
+            return [dict get $returned_message_d code]
+        }
+
+        return ""
+    }
+    namespace export read_result_code
+
+    proc read_result {} {
+        variable returned_message_d
+
+        return $returned_message_d
+    }
+    namespace export read_result
+
+    proc query_server {connection client_message expected_code {max_wait 20}} {
+        variable wait_v
+
+        send_to_server $connection $client_message $expected_code
+        set proto_status [read_protocol_status]
+
+        while {($proto_status == "READING") && ([incr nwait_t] < $max_wait)} { 
+            #puts $proto_status
+            vwait [namespace current]::wait_v 
+            set proto_status [read_protocol_status]
+        }
+        
+        if {$nwait_t >= $max_wait} {
+            return -code error -errorcode timeout_error "Timeout while reading from socket"
+        }
+
+        return [read_result]
+    }
+    namespace export query_server
+    namespace ensemble create
+}
+
+package provide ngis::clientio 1.0
