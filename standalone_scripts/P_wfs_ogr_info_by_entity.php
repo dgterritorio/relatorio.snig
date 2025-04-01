f83a1e0758529412a7b0d60c567cba55bbcb5e40
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-04-01 13:32:43 +0100

    Create P_wfs_ogr_info_by_entity.php

---
+++ standalone_scripts/P_wfs_ogr_info_by_entity.php
@@ -0,0 +1,59 @@
+<?php
+// Get the folder where the script is saved
+$script_dir = dirname(__FILE__);
+$params_file = $script_dir . '/connection_parameters.txt';
+
+// Load connection parameters from file
+$params = parse_ini_file($params_file);
+
+$host = $params['HOST'];
+$port = '5432';
+$dbname = $params['DB_NAME'];
+$user = $params['USERNAME'];
+$password = $params['PASSWORD'];
+
+// Connect to the PostgreSQL database
+$conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
+
+if (!$conn) {
+    die("Connection failed: " . pg_last_error());
+}
+
+// Query the view
+$query = 'SELECT gid, entity, status_code, definition, count, ping_average FROM stats_and_metrics._14_group_by_wfs_ogr_info_validity_and_entity';
+$result = pg_query($conn, $query);
+
+if (!$result) {
+    die("Query failed: " . pg_last_error());
+}
+
+// Render the body HTML
+header('Content-Type: text/html');
+?>
+
+<b style="font-size: 24px;">WFS OGR_INFO RESPONSE by Entity</b>
+
+<table border="1" cellpadding="5" cellspacing="0">
+    <tr>
+        <th>Entity</th>
+        <th>Status Code</th>
+        <th>Definition</th>
+        <th>Count</th>
+        <th>Ping Average</th>
+    </tr>
+
+    <?php while ($row = pg_fetch_assoc($result)): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($row['entity']); ?></td>
+            <td><?php echo htmlspecialchars($row['status_code']); ?></td>
+            <td><?php echo htmlspecialchars($row['definition']); ?></td>
+            <td><?php echo htmlspecialchars($row['count']); ?></td>
+            <td><?php echo htmlspecialchars(number_format((float)$row['ping_average'], 2)); ?></td>
+        </tr>
+    <?php endwhile; ?>
+</table>
+
+<?php
+// Close the connection
+pg_close($conn);
+?>
