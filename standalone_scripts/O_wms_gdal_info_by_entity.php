55388296df3017127a3d2a25491cdcde837a213f
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-04-01 18:08:31 +0100

    Update O_wms_gdal_info_by_entity.php

--- standalone_scripts/O_wms_gdal_info_by_entity.php
+++ standalone_scripts/O_wms_gdal_info_by_entity.php
@@ -31,7 +31,7 @@ if (!$result) {
 header('Content-Type: text/html');
 ?>
 
-<b style="font-size: 24px;"><a href="/pages/_13_group_by_wms_gdal_info_validity_and_entity.csv">WMS GDAL_INFO RESPONSE by Entity</a>
+<b style="font-size: 24px;"><a href="/pages/_13_group_by_wms_gdal_info_validity_and_entity.csv">WMS GDAL_INFO RESPONSE by Entity</a></b>
 <br/><br/>
 <table>
     <tr>
