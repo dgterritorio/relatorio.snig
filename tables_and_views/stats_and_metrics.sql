2ed4689339545bf589ccab0b0e2225f257d90bd3
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-04-08 07:10:33 +0100

    Update stats_and_metrics.sql

--- tables_and_views/stats_and_metrics.sql
+++ tables_and_views/stats_and_metrics.sql
@@ -24,61 +24,54 @@ CREATE OR REPLACE VIEW stats_and_metrics._02_group_by_http_status_code_global
  WITH a AS (
          SELECT
                 CASE
-                	WHEN service_status.exit_info ~~ 'http_status_code: 200%'::text THEN '200'::text
-			WHEN service_status.exit_info = 'Invalid HTTP status code 0'::text THEN 'Status code 0'::text
-               		WHEN service_status.exit_info = 'Failure in receiving network data'::text THEN 'Network error'::text
-               		WHEN service_status.exit_info = 'Failed to connect to host'::text THEN 'Cannot connect to host'::text
-               		WHEN service_status.exit_info = 'Peer certificate cannot be authenticated with known CA certificates'::text THEN 'Certificates error'::text
-               		WHEN service_status.exit_info ~~ 'Success with http code 200 after redir%'::text THEN '200 after 301/302 redirect'::text
-               		WHEN service_status.exit_info = 'Invalid HTTP status code 301 after redir'::text THEN 'Error HTTP status code after redirect'::text
-               		WHEN service_status.exit_info = 'Invalid HTTP status code 302 after redir'::text THEN 'Error HTTP status code after redirect'::text
-                   	WHEN 
-               		service_status.exit_info ~~ 'Invalid HTTP status code%'::text 
-               		AND
-               		service_status.exit_info != 'Invalid HTTP status code 301 after redir'::text
-               		AND
-               		service_status.exit_info != 'Invalid HTTP status code 302 after redir'::text
-               		THEN "right"(service_status.exit_info, 3)
+		    WHEN service_status.exit_info = 'Curl got nothing from the server'::text THEN 'Empty response'::text
+		    WHEN service_status.exit_info = 'An error occurred during the SSL/TLS handshake'::text THEN 'SSL error'::text
+                    WHEN service_status.exit_info ~~ 'http_status_code: 200%'::text THEN '200'::text
+                    WHEN service_status.exit_info = 'Invalid HTTP status code 0'::text THEN 'Status code 0'::text
+                    WHEN service_status.exit_info = 'Failure in receiving network data'::text THEN 'Network error'::text
+                    WHEN service_status.exit_info = 'Failed to connect to host'::text THEN 'Cannot connect to host'::text
+                    WHEN service_status.exit_info = 'Peer certificate cannot be authenticated with known CA certificates'::text THEN 'Certificates error'::text
+                    WHEN service_status.exit_info ~~ 'Success with http code 200 after redir%'::text THEN '200 after 301/302 redirect'::text
+                    WHEN service_status.exit_info = 'Invalid HTTP status code 301 after redir'::text THEN 'Error HTTP status code after redirect'::text
+                    WHEN service_status.exit_info = 'Invalid HTTP status code 302 after redir'::text THEN 'Error HTTP status code after redirect'::text
+                    WHEN service_status.exit_info ~~ 'Invalid HTTP status code%'::text AND service_status.exit_info <> 'Invalid HTTP status code 301 after redir'::text AND service_status.exit_info <> 'Invalid HTTP status code 302 after redir'::text THEN "right"(service_status.exit_info, 3)
                     ELSE service_status.exit_info
                 END AS status_code,
             count(*) AS count,
-			avg(service_status.task_duration) AS ping_average
+            avg(service_status.task_duration) AS ping_average
            FROM testsuite.service_status
           WHERE service_status.task::text = 'url_status_codes'::text
           GROUP BY (
                 CASE
+		    WHEN service_status.exit_info = 'Curl got nothing from the server'::text THEN 'Empty response'::text
+		    WHEN service_status.exit_info = 'An error occurred during the SSL/TLS handshake'::text THEN 'SSL error'::text
                     WHEN service_status.exit_info ~~ 'http_status_code: 200%'::text THEN '200'::text
-               	    WHEN service_status.exit_info = 'Invalid HTTP status code 0'::text THEN 'Status code 0'::text
-               	    WHEN service_status.exit_info = 'Failure in receiving network data'::text THEN 'Network error'::text
+                    WHEN service_status.exit_info = 'Invalid HTTP status code 0'::text THEN 'Status code 0'::text
+                    WHEN service_status.exit_info = 'Failure in receiving network data'::text THEN 'Network error'::text
                     WHEN service_status.exit_info = 'Failed to connect to host'::text THEN 'Cannot connect to host'::text
-               	    WHEN service_status.exit_info = 'Peer certificate cannot be authenticated with known CA certificates'::text THEN 'Certificates error'::text					
-               	    WHEN service_status.exit_info ~~ 'Success with http code 200 after redir%'::text THEN '200 after 301/302 redirect'::text
-               	    WHEN service_status.exit_info = 'Invalid HTTP status code 301 after redir'::text THEN 'Error HTTP status code after redirect'::text
+                    WHEN service_status.exit_info = 'Peer certificate cannot be authenticated with known CA certificates'::text THEN 'Certificates error'::text
+                    WHEN service_status.exit_info ~~ 'Success with http code 200 after redir%'::text THEN '200 after 301/302 redirect'::text
+                    WHEN service_status.exit_info = 'Invalid HTTP status code 301 after redir'::text THEN 'Error HTTP status code after redirect'::text
                     WHEN service_status.exit_info = 'Invalid HTTP status code 302 after redir'::text THEN 'Error HTTP status code after redirect'::text
-                    WHEN 
-               	    service_status.exit_info ~~ 'Invalid HTTP status code%'::text 
-               	    AND
-               	    service_status.exit_info != 'Invalid HTTP status code 301 after redir'::text
-               	    AND
-               	    service_status.exit_info != 'Invalid HTTP status code 302 after redir'::text
-               	    THEN "right"(service_status.exit_info, 3)
-               	    ELSE service_status.exit_info
+                    WHEN service_status.exit_info ~~ 'Invalid HTTP status code%'::text AND service_status.exit_info <> 'Invalid HTTP status code 301 after redir'::text AND service_status.exit_info <> 'Invalid HTTP status code 302 after redir'::text THEN "right"(service_status.exit_info, 3)
+                    ELSE service_status.exit_info
                 END)
         ), temp AS (
          SELECT row_number() OVER () AS gid,
             a.status_code,
                 CASE a.status_code
-				    
+		    WHEN 'Empty response'::text THEN 'Empty response'::text
+		    WHEN 'SSL error'::text THEN 'SSL error'::text
                     WHEN '000'::text THEN 'Timeout'::text
-		    WHEN 'URL status code check failed on a 20 secs timeout error'::text THEN 'Timeout'::text
+                    WHEN 'URL status code check failed on a 20 secs timeout error'::text THEN 'Timeout'::text
                     WHEN '200'::text THEN 'OK'::text
-		    WHEN '200 after 301/302 redirect'::text THEN 'OK after redirect'::text
+                    WHEN '200 after 301/302 redirect'::text THEN 'OK after redirect'::text
                     WHEN '201'::text THEN 'Created'::text
                     WHEN '202'::text THEN 'Accepted'::text
                     WHEN '204'::text THEN 'No Content'::text
                     WHEN '301'::text THEN 'Moved Permanently'::text
                     WHEN '302'::text THEN 'Found'::text
-		    WHEN 'Error HTTP status code after redirect'::text THEN 'Error after redirect'::text
+                    WHEN 'Error HTTP status code after redirect'::text THEN 'Error after redirect'::text
                     WHEN '400'::text THEN 'Bad Request'::text
                     WHEN '401'::text THEN 'Unauthorized'::text
                     WHEN '403'::text THEN 'Forbidden'::text
@@ -88,11 +81,11 @@ CREATE OR REPLACE VIEW stats_and_metrics._02_group_by_http_status_code_global
                     WHEN '503'::text THEN 'Service Unavailable'::text
                     WHEN '504'::text THEN 'Gateway Timeout'::text
                     WHEN '499'::text THEN 'Client Closed Request'::text
-               	    WHEN 'Error resolving the URL host name'::text THEN 'Hostname unkwown'::text
-               	    WHEN 'Network error'::text THEN 'Network error'::text
-               	    WHEN 'Cannot connect to host'::text THEN 'Cannot connect to host'::text
-               	    WHEN 'Certificates error'::text THEN 'Certificates error'::text
-               	    WHEN 'Status code 0'::text THEN 'To be investigated'::text
+                    WHEN 'Error resolving the URL host name'::text THEN 'Hostname unkwown'::text
+                    WHEN 'Network error'::text THEN 'Network error'::text
+                    WHEN 'Cannot connect to host'::text THEN 'Cannot connect to host'::text
+                    WHEN 'Certificates error'::text THEN 'Certificates error'::text
+                    WHEN 'Status code 0'::text THEN 'To be investigated'::text
                     ELSE ''::text
                 END AS definition,
             a.count,
@@ -105,7 +98,7 @@ CREATE OR REPLACE VIEW stats_and_metrics._02_group_by_http_status_code_global
     count,
     ping_average
    FROM temp
-  ORDER BY count desc;
+  ORDER BY count DESC;
 
 -- Count the URLs by they http status code and group also by organization
 CREATE OR REPLACE VIEW stats_and_metrics._03_group_by_http_status_code_and_entity
