4ca577e0b88ec870d7ff9b46c1f7336f044b750a
Massimo Manghi <mxmanghi@apache.org>
2025-10-23 18:45:46 +0200

    Review metrics queries

--- tables_and_views/metrics.sql
+++ tables_and_views/metrics.sql
@@ -37,46 +37,43 @@ ranked AS (
     a.eid,
     ROW_NUMBER() OVER (ORDER BY a.count DESC, a.status_code) AS rid,
     a.status_code,
-    COALESCE(l.definition, '') AS definition,
+    COALESCE(l.definition,'') AS status_code_definition,
     a.count,
     a.ping_average
   FROM agg a
   LEFT JOIN LATERAL testsuite.labels() l ON l.code = a.status_code
 )
-SELECT eid, rid, status_code, definition, count, ping_average FROM ranked ORDER BY eid, count DESC;
-
+SELECT eid, rid, status_code, status_code_definition, count, ping_average FROM ranked ORDER BY eid, count DESC;
 
 -- Count the URLs by they http status code and group also by domain
 CREATE OR REPLACE VIEW metrics._04_group_by_http_status_code_and_domain
  AS
  WITH a AS (
-         SELECT
-            c.eid, testsuite.exit_info_map(b.exit_info) AS status_code,
-            lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
-            count(*) AS count,
-            avg(b.task_duration) AS ping_average
-           FROM testsuite.service_status b
-             JOIN testsuite.uris_long c ON b.gid = c.gid
-          WHERE b.task::text = 'url_status_codes'::text
-          GROUP BY c.eid,uri_domain,status_code
-        ), temp AS (
-         SELECT row_number() OVER () AS rid,
-            a.eid,
-            a.uri_domain,
-            a.status_code,
-            l.definition as status_code_definition,
-            a.count,
-            a.ping_average
-           FROM a LEFT JOIN LATERAL testsuite.labels() l on l.code = a.status_code
-        )
+     SELECT
+        c.eid, testsuite.exit_info_map(b.exit_info) AS status_code,
+        lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
+        count(*) AS count,
+        avg(b.task_duration) AS ping_average
+        FROM testsuite.service_status b JOIN testsuite.uris_long c ON b.gid = c.gid
+        WHERE b.task::text = 'url_status_codes'::text
+        GROUP BY c.eid,uri_domain,status_code
+    ), temp AS (
+     SELECT row_number() OVER () AS rid,
+        a.eid,
+        a.uri_domain,
+        a.status_code,
+        COALESCE(l.definition,'') as status_code_definition,
+        a.count,
+        a.ping_average
+       FROM a LEFT JOIN LATERAL testsuite.labels() l on l.code = a.status_code
+    )
  SELECT rid, eid,
     uri_domain,
     status_code,
     status_code_definition,
     count,
     ping_average
-   FROM temp
-  ORDER BY uri_domain, count DESC;
+   FROM temp ORDER BY uri_domain, count DESC;
 
 
 -- Count the URLs by they WMS and WFS Capabilities document validity 
@@ -85,8 +82,8 @@ CREATE OR REPLACE VIEW metrics._05_group_by_wms_capabilities_validity_global
  WITH a AS (
          SELECT
             ul.eid,
-			ss.exit_info AS status_code,
-			ss.exit_status,
+			ss.exit_info AS result_message,
+			ss.exit_status AS result_code,
             count(*) AS count,
 			avg(ss.task_duration) AS ping_average
           FROM testsuite.service_status AS ss
@@ -95,15 +92,15 @@ CREATE OR REPLACE VIEW metrics._05_group_by_wms_capabilities_validity_global
           GROUP BY ul.eid, ss.exit_info, ss.exit_status
         ), temp AS (
          SELECT a.eid, row_number() OVER () AS rid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.result_code,
+            a.result_message,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
    FROM temp
@@ -115,8 +112,8 @@ CREATE OR REPLACE VIEW metrics._06_group_by_wfs_capabilities_validity_global
  WITH a AS (
          SELECT
             ul.eid,
-			ss.exit_info AS status_code,
-			ss.exit_status,
+			ss.exit_info AS result_message,
+			ss.exit_status AS result_code,
             count(*) AS count,
 			avg(ss.task_duration) AS ping_average
           FROM testsuite.service_status AS ss
@@ -125,19 +122,19 @@ CREATE OR REPLACE VIEW metrics._06_group_by_wfs_capabilities_validity_global
           GROUP BY ul.eid, ss.exit_info, ss.exit_status 
         ), temp AS (
          SELECT a.eid, row_number() OVER () AS rid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.result_code,
+            a.result_message,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
-   FROM temp
-  ORDER BY eid, count desc;
+ FROM temp
+ ORDER BY eid, count desc;
 
 -- Count the URLs by they WMS/WFS Capabilities XML document validity and group also by organization
 CREATE OR REPLACE VIEW metrics._07_group_by_wms_capabilities_validity_and_entity
@@ -145,8 +142,8 @@ CREATE OR REPLACE VIEW metrics._07_group_by_wms_capabilities_validity_and_entity
  WITH a AS (
          SELECT
             c.eid,
-			b.exit_info AS status_code,
-			b.exit_status,
+			b.exit_info AS result_message,
+			b.exit_status AS result_code,
             count(*) AS count,
 			avg(b.task_duration) AS ping_average
           FROM testsuite.service_status b
@@ -156,26 +153,25 @@ CREATE OR REPLACE VIEW metrics._07_group_by_wms_capabilities_validity_and_entity
         ), temp AS (
          SELECT row_number() OVER () AS rid,
             a.eid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.result_code,
+            a.result_message,
             a.count,
             a.ping_average
            FROM a
         )
- SELECT rid, eid,
-    status_code,
-    definition,
-    count,
-    ping_average
-   FROM temp
-  ORDER BY eid, count desc;
+     SELECT rid, eid,
+            result_code,
+            result_message,
+            count,
+            ping_average
+            FROM temp ORDER BY eid, count desc;
 
 CREATE OR REPLACE VIEW metrics._08_group_by_wfs_capabilities_validity_and_entity
  AS
  WITH a AS (
          SELECT
-			b.exit_info AS status_code,
-			b.exit_status,
+			b.exit_info AS result_message,
+			b.exit_status AS result_code,
 			c.eid,
             count(*) AS count,
 			avg(b.task_duration) AS ping_average
@@ -186,16 +182,16 @@ CREATE OR REPLACE VIEW metrics._08_group_by_wfs_capabilities_validity_and_entity
         ), temp AS (
          SELECT row_number() OVER () AS rid,
 		    a.eid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.result_code,
+            a.result_message,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid,
         eid,
-        status_code,
-        definition,
+        result_code,
+        result_message,
         count,
         ping_average
    FROM temp
@@ -205,7 +201,7 @@ CREATE OR REPLACE VIEW metrics._08_group_by_wfs_capabilities_validity_and_entity
 CREATE OR REPLACE VIEW metrics._09_group_by_wms_capabilities_validity_and_domain
  AS
  WITH a AS (
-         SELECT b.exit_info AS status_code,
+         SELECT b.exit_info,
             b.exit_status, c.eid,
             lower(regexp_replace(regexp_replace(c.uri_original,'^https?://'::text, ''::text),'(:[0-9]+)?/.*$'::text,''::text)) AS uri_domain,
             count(*) AS count,
@@ -218,16 +214,16 @@ CREATE OR REPLACE VIEW metrics._09_group_by_wms_capabilities_validity_and_domain
          SELECT row_number() OVER () AS rid,
             a.eid,
             a.uri_domain,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
     uri_domain,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
    FROM temp
@@ -237,40 +233,37 @@ CREATE OR REPLACE VIEW metrics._09_group_by_wms_capabilities_validity_and_domain
 CREATE OR REPLACE VIEW metrics._10_group_by_wfs_capabilities_validity_and_domain
  AS
  WITH a AS (
-         SELECT b.exit_info AS status_code,
-            b.exit_status, c.eid,
-            lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
-            count(*) AS count,
-            avg(b.task_duration) AS ping_average
-           FROM testsuite.service_status b
-             JOIN testsuite.uris_long c ON b.gid = c.gid
-          WHERE b.task::text = 'wfs_capabilities'::text
-          GROUP BY c.eid,uri_domain, b.exit_info, b.exit_status
+    SELECT b.exit_info, b.exit_status, c.eid,
+       lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
+           count(*) AS count,
+           avg(b.task_duration) AS ping_average
+           FROM testsuite.service_status b JOIN testsuite.uris_long c ON b.gid = c.gid
+           WHERE b.task::text = 'wfs_capabilities'::text
+           GROUP BY c.eid,uri_domain, b.exit_info, b.exit_status
         ), temp AS (
          SELECT row_number() OVER () AS rid,
             a.eid,
             a.uri_domain,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
     uri_domain,
-    status_code,
-    definition,
+    result_message,
+    result_code,
     count,
     ping_average
-   FROM temp
-  ORDER BY uri_domain, count DESC;
+ FROM temp ORDER BY uri_domain, count DESC;
 
 
 -- Count the URLs by they WMS and WFS gdal_info/ogr_info response validity 
 CREATE OR REPLACE VIEW metrics._11_group_by_wms_gdal_info_validity_global
  AS
  WITH a AS (
-         SELECT ul.eid,ss.exit_info AS status_code,
+         SELECT ul.eid,ss.exit_info,
             ss.exit_status,
             count(*) AS count,
             avg(ss.task_duration) AS ping_average
@@ -281,15 +274,15 @@ CREATE OR REPLACE VIEW metrics._11_group_by_wms_gdal_info_validity_global
         ), temp AS (
          SELECT row_number() OVER () AS rid,
             a.eid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid, (SELECT ent.description FROM testsuite.entities AS ent WHERE ent.eid=temp.eid) entity,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
    FROM temp
@@ -307,26 +300,26 @@ CREATE OR REPLACE VIEW metrics._12_group_by_wfs_ogr_info_validity_global
                 WHEN ss.exit_info = 'Service exception or error'::text THEN 'Service exception or error'::text
                 WHEN ss.exit_info ~~ 'Service exception or error (%'::text THEN 'Non fatal exception/error'::text
                 ELSE ss.exit_info
-            END AS status_code,
+            END, 
             ss.exit_status,
             count(*) AS count,
            avg(ss.task_duration) AS ping_average
            FROM testsuite.service_status AS ss
            JOIN testsuite.uris_long AS ul ON ul.gid = ss.gid
           WHERE ss.task::text = 'wfs_ogr_info'::text
-          GROUP BY ul.eid,status_code, ss.exit_status
+          GROUP BY ul.eid, ss.exit_status, ss.exit_info
     ), temp AS (
          SELECT row_number() OVER () AS rid,
             a.eid,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid, (SELECT ent.description FROM testsuite.entities AS ent WHERE ent.eid=temp.eid) entity,
-    status_code,
-    definition,
+    result_message,
+    result_code,
     count,
     ping_average
    FROM temp
@@ -336,7 +329,7 @@ CREATE OR REPLACE VIEW metrics._12_group_by_wfs_ogr_info_validity_global
 CREATE OR REPLACE VIEW metrics._13_group_by_wms_gdal_info_validity_and_entity
  AS
  WITH a AS (
-         SELECT b.exit_info AS status_code,
+         SELECT b.exit_info,
             b.exit_status,
             c.eid,
             count(*) AS count,
@@ -349,8 +342,8 @@ CREATE OR REPLACE VIEW metrics._13_group_by_wms_gdal_info_validity_and_entity
          SELECT row_number() OVER () AS rid,
             a.eid,
             (SELECT entities.description FROM testsuite.entities WHERE entities.eid=a.eid) entity,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
@@ -358,8 +351,8 @@ CREATE OR REPLACE VIEW metrics._13_group_by_wms_gdal_info_validity_and_entity
  SELECT rid,
     eid,
     entity,
-    status_code,
-    definition,
+    result_message,
+    result_code,
     count,
     ping_average
    FROM temp
@@ -368,7 +361,7 @@ CREATE OR REPLACE VIEW metrics._13_group_by_wms_gdal_info_validity_and_entity
 CREATE OR REPLACE VIEW metrics._14_group_by_wfs_ogr_info_validity_and_entity
     AS
     WITH a AS (
-         SELECT b.exit_info AS status_code,
+         SELECT b.exit_info,
             b.exit_status,
             c.eid,
             count(*) AS count,
@@ -381,16 +374,16 @@ CREATE OR REPLACE VIEW metrics._14_group_by_wfs_ogr_info_validity_and_entity
          SELECT row_number() OVER () AS rid,
             a.eid,
             (SELECT entities.description FROM testsuite.entities WHERE entities.eid=a.eid) entity,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
     SELECT  rid,
             entity,
-            status_code,
-            definition,
+            result_code,
+            result_message,
             count,
             ping_average
    FROM temp
@@ -400,9 +393,9 @@ CREATE OR REPLACE VIEW metrics._14_group_by_wfs_ogr_info_validity_and_entity
 CREATE OR REPLACE VIEW metrics._15_group_by_wms_gdal_info_validity_and_domain
  AS
  WITH a AS (
-         SELECT b.exit_info AS status_code,
-            c.eid,
+         SELECT b.exit_info,
             b.exit_status,
+            c.eid,
             lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
             count(*) AS count,
             avg(b.task_duration) AS ping_average
@@ -413,16 +406,16 @@ CREATE OR REPLACE VIEW metrics._15_group_by_wms_gdal_info_validity_and_domain
          SELECT row_number() OVER () AS rid,
             a.eid,
             a.uri_domain,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
     uri_domain,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
    FROM temp
@@ -431,9 +424,9 @@ CREATE OR REPLACE VIEW metrics._15_group_by_wms_gdal_info_validity_and_domain
 CREATE OR REPLACE VIEW metrics._16_group_by_wfs_ogr_info_validity_and_domain
  AS
  WITH a AS (
-         SELECT b.exit_info AS status_code,
-            c.eid,
+         SELECT b.exit_info,
             b.exit_status,
+            c.eid,
             lower(regexp_replace(regexp_replace(c.uri_original, '^https?://'::text, ''::text), '(:[0-9]+)?/.*$'::text, ''::text)) AS uri_domain,
             count(*) AS count,
             avg(b.task_duration) AS ping_average
@@ -444,16 +437,16 @@ CREATE OR REPLACE VIEW metrics._16_group_by_wfs_ogr_info_validity_and_domain
          SELECT row_number() OVER () AS rid,
             a.eid,
             a.uri_domain,
-            a.status_code,
-            a.exit_status AS definition,
+            a.exit_info AS result_message,
+            a.exit_status AS result_code,
             a.count,
             a.ping_average
            FROM a
         )
  SELECT rid, eid,
     uri_domain,
-    status_code,
-    definition,
+    result_code,
+    result_message,
     count,
     ping_average
    FROM temp
