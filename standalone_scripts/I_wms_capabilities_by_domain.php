46cb35732b7e6a7684460b38250846a769bd3c00
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-10-27 08:30:08 +0000

    Fixes https://github.com/dgterritorio/relatorio.snig/issues/22

--- standalone_scripts/I_wms_capabilities_by_domain.php
+++ standalone_scripts/I_wms_capabilities_by_domain.php
@@ -35,7 +35,7 @@ header('Content-Type: text/html');
 <br/><br/>
 <table>
     <tr>
-        <th>Entity</th>
+        <th>Domain</th>
         <th>Status Code</th>
         <th>Definition</th>
         <th>Count</th>
