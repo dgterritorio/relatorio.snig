6494712b4c9f835a0b63f993997fd39ef36c7c93
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-11-06 15:54:08 +0000

    Improve indentation and comments

--- standalone_scripts/01_download_csw_records.sh
+++ standalone_scripts/01_download_csw_records.sh
@@ -3,12 +3,13 @@
 BASEFOLDER="/tmp"
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 
-# Export the variables so that it's available in the parallel processes
+# Export the variables so that they're available in the parallel processes
 export BASEFOLDER
 export SCRIPT_DIR
 
 # Remove existing files
 rm -f "$BASEFOLDER/CSW_RECORDS/*.xml"
+
 # Ensure necessary directory exists
 mkdir -p "$BASEFOLDER/CSW_RECORDS"
 
@@ -19,11 +20,11 @@ process_chunk() {
 
     echo "Processing $step records starting at $start"
 
-    # Unique payload file for this chunk
+    # Unique payload files for this chunk
     local payload_file="download_csw_records_payload_${start}.txt"
     local payload_file_cdgs="download_csw_records_payload_cdgs_${start}.txt"
 
-    # Prepare payload file
+    # Prepare payload files
     cp "$SCRIPT_DIR/csw_payloads/01_download_csw_records_payload_template.txt" "$BASEFOLDER/$payload_file"
     cp "$SCRIPT_DIR/csw_payloads/01_download_csw_records_payload_cdgs_template.txt" "$BASEFOLDER/$payload_file_cdgs"
 
@@ -42,21 +43,21 @@ process_chunk() {
         "https://snig.dgterritorio.gov.pt/rndg/srv/eng/csw-inspire?request=GetRecords&service=CSW" \
         -o "$BASEFOLDER/CSW_RECORDS/csw_records_inspire_${start}_$((start + step)).xml"
 
-    # Harvest also CDGS catalog
+    # Harvest also the CDGS catalog
     curl -X POST --header "Content-Type:text/xml;charset=UTF-8" \
         --data @"$BASEFOLDER/$payload_file_cdgs" \
         "https://snig.dgterritorio.gov.pt/rndg/srv/por/csw-cdgs?request=GetRecords&service=CSW" \
         -o "$BASEFOLDER/CSW_RECORDS/csw_records_cdgs_${start}_$((start + step)).xml"
 
-    # Cleanup temporary file
+    # Cleanup temporary files
     rm -f "$BASEFOLDER/$payload_file" "$BASEFOLDER/$payload_file_cdgs"
 }
 
 # Export the function for use with GNU parallel
 export -f process_chunk
 
-# Remove existing files
-#rm -f download_csw_records_payload_*.txt
+# Remove existing payload files
+# rm -f download_csw_records_payload_*.txt
 
 # Generate sequence of starting indices and run in parallel
 seq 1 10 18000 | parallel --jobs 4 process_chunk {}
