bbe1c9e0e98c7cbd9989afdcb7711a383a23c717
Massimo Manghi <mxmanghi@apache.org>
2025-11-10 17:28:54 +0100

    Missing latest changes to ngisutils.tcl

--- monitor/tcl/ngisutils.tcl
+++ monitor/tcl/ngisutils.tcl
@@ -11,27 +11,33 @@ namespace eval ::ngis::utils {
 
     array set msg_templates [list hash_changed {
     Message for MANAGER (EMAIL)
+
+    SENDER_NAME
 }]
 
     proc send_mail {template data_d} {
         variable msg_templates
         dict with data_d {
-            if {[info exists email]   &&  \
-                [info exists manager] &&  \
-                [info exists hash]} {
-
+            if {[dict exists $msg_templates $template]} {
                 set msg [dict get $msg_templates $template]
-                set msg [string map [list MANAGER       $manager    \
-                                          EMAIL         $email      \
-                                          SENDER_NAME   $::ngis::manager_name \
-                                          SENDER_EMAIL  $::ngis::manager_email] $msg]
+            }
 
+            switch $template {
+                hash_changed {
+                    if {[info exists email]   &&  \
+                        [info exists manager] &&  \
+                        [info exists hash]} {
 
+                        set msg [string map [list MANAGER       $manager    \
+                                                  EMAIL         $email      \
+                                                  SENDER_NAME   $::ngis::manager_name \
+                                                  SENDER_EMAIL  $::ngis::manager_email] $msg]
 
-
-                return -code ok
-            } else {
-                return -code error -errorcode missing_template_argument "Missing argument to fill template"
+                        return -code ok
+                    } else {
+                        return -code error -errorcode missing_template_argument "Missing argument to fill template"
+                    }
+                }
             }
         }
     }
