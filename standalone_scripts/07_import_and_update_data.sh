e5a2c47b7c9f7b9b4fd8de38d44da204b2d525a0
Giovanni Manghi <giovanni.manghi@naturalgis.pt>
2025-12-04 16:46:18 +0000

    Update 07_import_and_update_data.sh

--- standalone_scripts/07_import_and_update_data.sh
+++ standalone_scripts/07_import_and_update_data.sh
@@ -10,26 +10,16 @@ source "$SCRIPT_DIR/connection_parameters.txt"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "TRUNCATE TABLE testsuite.uris_wide;"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "ALTER SEQUENCE testsuite.uris_wide_gid_seq RESTART WITH 1;"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "\COPY testsuite.uris_wide(uuid,entity,description,uri1,uri2,uri3,uri4,uri5,uri6,uri7,uri8,uri9,uri10,uri11,uri12,uri13) FROM $BASEFOLDER/geonetwork_records_urls_wide.csv DELIMITER '$' CSV;"
-
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "TRUNCATE TABLE testsuite.uris_long_temp;"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "ALTER SEQUENCE testsuite.uris_long_temp_gid_seq RESTART WITH 1;"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "\COPY testsuite.uris_long_temp(uuid,entity,description,uri_original,uri_type,version,uri) FROM $BASEFOLDER/geonetwork_records_urls_long_with_type.csv DELIMITER '$' CSV;"
 
-# In the \"long\" temp table copy to the proper column, for records that are not OGC services, the URLs to be tested
+# In the \"long\" temp table copy to the proper column the URLs to be tested (for records that are not OGC services)
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "UPDATE testsuite.uris_long_temp SET uri=uri_original WHERE uri IS NULL;"
-
-# Use the records in \"uris_long_temp\" to delete from \"uris_long\" the recorsds with UUIDs/uris that do not exist anymore or
-# uris that have changed, then import in \"uris_long\" from \"uris_long_temp\" the records corresponding to UUIDs/uris that are missing
-
-# NOTE: removed records from testsuite.uris_long, log and status tables will
-# be kept in history tables, that will be populated with TRIGGERS, not with queries
-# as initially done. The queries here below can be optimized (ON CASCADE...) but for sake of clarity now
-# are written in a more basic way
 
 # Find records in testsuite.uris_long and status and log tables that have UUIDs that are not existing anymore
 # among the ones that are in the testsuite.uris_long_temp, which is the result of a new catalog harvest,
 # and delete them
-
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "CREATE TABLE temp_table AS \
 SELECT DISTINCT uuid FROM testsuite.uris_long a \
 WHERE a.uuid NOT IN (SELECT DISTINCT b.uuid FROM testsuite.uris_long_temp b);"
@@ -57,7 +47,7 @@ WHERE uri_original IN (SELECT uri_original FROM temp_table);"
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "DROP TABLE temp_table;"
 
 
-# Now possibly a number of records with some specific gid have been removed from testsuite.uris_long
+# (Possibly) records with some specific gid have been removed from testsuite.uris_long
 # so we remove the entries with the same gid from the status and log tables
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "DELETE FROM testsuite.service_log a \
 WHERE a.gid NOT IN (SELECT DISTINCT b.gid FROM testsuite.uris_long b);"
@@ -66,7 +56,7 @@ psql -U $USERNAME -d $DB_NAME -h $HOST -c "DELETE FROM testsuite.service_status 
 WHERE a.gid NOT IN (SELECT DISTINCT b.gid FROM testsuite.uris_long b);"
 
 
-# Add in testsuite.uris_long all the new records (by uuid and uri_original) that are now
+# Add in testsuite.uris_long all the new records (by uuid and uri_original) that are
 # in testsuite.uris_long_temp because of the new harvest
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "INSERT INTO testsuite.uris_long ( \
     uuid, \
@@ -112,16 +102,15 @@ SELECT \
 FROM (SELECT * FROM testsuite.uris_long_temp a \
 WHERE a.uri_original NOT IN (SELECT DISTINCT b.uri_original FROM testsuite.uris_long b));"
 
-# Update the entities table, with new entities that may have appeared
-# after a new harvest
+
+# Update the entities table, with new entities that may have appeared after a new harvest
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "INSERT INTO testsuite.entities (description) \
 SELECT DISTINCT entity FROM testsuite.uris_long_temp \
 WHERE entity IS NOT NULL AND entity NOT IN (SELECT description FROM testsuite.entities \
 WHERE description IS NOT NULL);"
 
 
-# Update the testsuite.uris_long table so to fill the empty eid values
-# in case of reocrds belonging to a new entity
+# Update the testsuite.uris_long table so to fill the empty eid values for the records belonging to a new entity
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "UPDATE testsuite.uris_long a \
 SET eid = b.eid \
 FROM testsuite.entities b \
@@ -135,13 +124,13 @@ WHERE a.eid IS NULL \
 AND b.description IS NULL;"
 
 
-# While in testsuite.uris_long_temp we NEED / WANT to keep reords with the same uri_original,
-# becase we NEED to compute stats about the levels of redundancy in the catalog,
+# In testsuite.uris_long_temp we NEED / WANT to keep records with the same uri_original,
+# because we NEED to compute stats about the levels of redundancy in the catalog,
 # in testsuite.uris_long we DON\'T want to have repeated uri, because we DON'\T want to test
 # over and over the same uri even if it belongs to different UUIDs. We DON\'T care if a specific
-# uri is tested for a specific UUID or another. The entity for a specific uri remain the same,
+# uri is tested for a specific UUID or another. The entity responsible for a specific uri remain the same,
 # and while the decription of the UUID may change, we also DON'T care. If a specific uri works or does not
-# the results apply to all UUIDs of the catalog where it was added. So if it does not work, once fixed,
+# the results apply to all UUIDs of the catalog where it was added. So if it does not work, once is fixed,
 # all are fixed.
 
 psql -U $USERNAME -d $DB_NAME -h $HOST -c "WITH duplicates AS ( \
@@ -154,6 +143,3 @@ WHERE ctid IN ( \
     FROM duplicates \
     WHERE rn > 1 \
     );"
-
-# Remove from testsuite.service_status the records corresponding to URLs that have disappeared after a new harvest
-psql -U $USERNAME -d $DB_NAME -h $HOST -c "DELETE FROM testsuite.service_status WHERE gid NOT IN (SELECT DISTINCT gid FROM testsuite.uris_long);"
