82aaecd821923d3ce0a2c9337c70c769103ea3a6
Massimo Manghi <mxmanghi@apache.org>
2025-12-09 23:15:50 +0100

    add RFC 6570 template URI tests

--- monitor/tasks/00_congruence.tcl
+++ monitor/tasks/00_congruence.tcl
@@ -21,9 +21,21 @@
                 uri_original -
                 uri {
                     # regexp simplified to avoid to check case sensitive cases
-                    set uri_re {^(((ht|f)tp(s?))\:\/\/)((\d+\.\d+\.\d+\.\d+)|([a-z0-9\-]+\.)*[a-z]+)(\:[0-9]+)*(\/($|[a-z0-9\.\,\;\?\'\\\+&%\$#\=~_\-]+))*$}
+
+                    set rfc3986_uri_re {^(((ht|f)tp(s?))\:\/\/)((\d+\.\d+\.\d+\.\d+)|([a-z0-9\-]+\.)*[a-z]+)(\:[0-9]+)*(\/($|[a-z0-9\.\,\;\?\'\\\+&%\$#\=~_\-]+))*$}
                     if {[dict exists $job_d $p]} {
-                        if {[regexp -nocase $uri_re [dict get $job_d $p]] == 0} {
+                        set uri [dict get $job_d $p]
+
+
+                        if {[regexp -nocase $rfc3986_uri_re $uri] == 0} {
+
+                            # let's see if it might contain the {...} arguments of a RFC 6570 template 
+                            set rfc6570_args [regexp -all {\/\{[a-zA-Z0-9_]\}} $uri]
+                            if {$rfc6570_args == 3} {
+                                # yes, it looks like we are checking a RFC 6570 template.
+                                return [::ngis::tasks::make_warning_result "possible_RFC6570_template" "" "Possible RFC 6570 URI"]
+                            }
+
                             return [::ngis::tasks::make_error_result "invalid_uri" "Invalid URI"]
                         }
                     } else {
